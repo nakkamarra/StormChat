7eb6d72caad3ee70f4fbba98706e4c8e1c22b2a9
Nick Brandt <nick@bigblueocean.net>
2017-11-08 14:04:05 -0500

    Start

--- app/src/main/java/com/stjohns/stormchat/Activities/HomeActivity.java
+++ app/src/main/java/com/stjohns/stormchat/Activities/HomeActivity.java
@@ -2,14 +2,39 @@ package com.stjohns.stormchat.Activities;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.stjohns.stormchat.R;
 
 public class HomeActivity extends Activity {
+
+    private FirebaseAuth userAuthenticator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+        userAuthenticator = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = userAuthenticator.getCurrentUser();
+
+        Button logOutButton = (Button) findViewById(R.id.logout_button);
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userAuthenticator.signOut();
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                HomeActivity.this.finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/stjohns/stormchat/Activities/LoginActivity.java
+++ app/src/main/java/com/stjohns/stormchat/Activities/LoginActivity.java
@@ -1,7 +1,14 @@
 package com.stjohns.stormchat.Activities;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
 import com.google.android.gms.tasks.*;
 import com.google.firebase.auth.*;
 import com.stjohns.stormchat.R;
@@ -9,7 +16,6 @@ import com.stjohns.stormchat.R;
 public class LoginActivity extends Activity {
 
     private FirebaseAuth loginAuthenticator;
-    private Intent toHome = new Intent(this, HomeActivity.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +29,25 @@ public class LoginActivity extends Activity {
         super.onStart();
         FirebaseUser currentUser = loginAuthenticator.getCurrentUser();
         if (currentUser != null){
-            startActivity(toHome);
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
         }
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        Button createAccountButton = (Button) findViewById(R.id.create_account_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog(R.id.login_button).show();
+            }
+        });
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog(R.id.create_account_button).show();
+            }
+        });
     }
 
     public void createAccount(String email, String password){
@@ -33,8 +56,7 @@ public class LoginActivity extends Activity {
                     @Override
                     public void onComplete(Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Creation success
-
+                            loginAuthenticator.getInstance().getCurrentUser().sendEmailVerification();
                         } else {
                             // If sign in fails, display a message to the user.
                         }
@@ -42,19 +64,72 @@ public class LoginActivity extends Activity {
                 });
     }
 
-    public void signIn(String email, String password){
+    public void logIn(String email, String password){
         loginAuthenticator.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, switch activites
-                            startActivity(toHome);
-                            LoginActivity.this.finish();
+                            if (loginAuthenticator.getCurrentUser().isEmailVerified()){
+                                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                                LoginActivity.this.finish();
+                            }
+                            else{
+                                //Display user not email verified
+                            }
                         } else {
-                            // If sign in fails, display a message to the user.
+                            //Display sign in failed
                         }
                     }
                 });
     }
+
+    public AlertDialog createDialog(int id){
+        final EditText emailField = new EditText(this);
+        final EditText passwordField = new EditText(this);
+        emailField.setMaxLines(1);
+        passwordField.setMaxLines(1);
+
+        LinearLayout inputs = new LinearLayout(this);
+        inputs.setOrientation(LinearLayout.VERTICAL);
+        inputs.addView(emailField);
+        inputs.addView(passwordField);
+
+        AlertDialog.Builder inputDialogBuilder = new AlertDialog.Builder(LoginActivity.this);
+        inputDialogBuilder.setView(inputs);
+        switch (id){
+            case R.id.login_button:
+                inputDialogBuilder.setTitle("Log In");
+                inputDialogBuilder.setMessage("Please provide your credentials:");
+                inputDialogBuilder.setPositiveButton("Log In", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        logIn(emailField.getText().toString(), passwordField.getText().toString());
+                    }
+                });
+                break;
+            case R.id.create_account_button:
+                inputDialogBuilder.setTitle("Create Account");
+                inputDialogBuilder.setMessage("Use your St. John's University email to create an account." +
+                        "A verification email will be sent to the specified address. Once verified, you will" +
+                        "be able to sign in using your new credentials." );
+                inputDialogBuilder.setPositiveButton("Create Account", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        createAccount(emailField.getText().toString(), passwordField.getText().toString());
+                    }
+                });
+                break;
+        }
+
+        inputDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        return inputDialogBuilder.create();
+    }
+
 }
