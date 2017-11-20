a4a3e3c0b97b5bdd08a056223c961d7dfa5c920e
AswiniRamesh14 <31488751+AswiniRamesh14@users.noreply.github.com>
2017-11-20 08:14:23 -0500

    Just checking if it works...
    Added some methods to the chat object and tried out some xml code

--- app/src/main/java/com/stjohns/stormchat/Objects/Chat/Chat.java
+++ app/src/main/java/com/stjohns/stormchat/Objects/Chat/Chat.java
@@ -5,4 +5,35 @@ package com.stjohns.stormchat.Objects.Chat;
  */
 
 public class Chat {
+    private String message;
+    private String sender;
+    private String recipient;
+
+    private int mRecipientOrSenderStatus;
+    public Chat()
+    {
+
+    }
+    public Chat(String message, String sender, String recipient)
+    {
+        this.message=message;
+        this.recipient=recipient;
+        this.sender=sender;
+    }
+    public int getmRecipientOrSenderStatus()
+    {
+        return mRecipientOrSenderStatus;
+    }
+    public String getMessage()
+    {
+        return message;
+    }
+    public String getSender()
+    {
+        return sender;
+    }
+    public String getRecipient()
+    {
+        return recipient;
+    }
 }
