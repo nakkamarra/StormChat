ab0575eb3d00b0a8ba428e71b099f80854b17c8e
AswiniRamesh14 <31488751+AswiniRamesh14@users.noreply.github.com>
2017-12-02 15:07:57 -0500

    Small Changes

--- app/src/main/java/com/stjohns/stormchat/Activities/ProfileActivity.java
+++ app/src/main/java/com/stjohns/stormchat/Activities/ProfileActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -36,8 +35,7 @@ public class ProfileActivity extends Activity
     private static final int SELECT_FILE = 2;
     EditText userNameEditText, userStatusEditText, userCollegeEditText, userMajorEditText;
     ImageView userPic;
-    Button userChangeImage;
-    ImageButton saveProfilePage,backButton;
+    ImageButton userChangeImage, saveProfilePage,backButton;
     FirebaseAuth authUser;
     FirebaseAuth.AuthStateListener authUserListener;
     StorageReference storageRef;
