566eaf2779449be4cf3dcaff71dce9b137b19da2
Andrew Tucker <ajtucks9@gmail.com>
2017-12-06 11:41:15 -0500

    SearchMenuXML
    Search Menu XML reference, along with a onCreate within the Search Activity

--- app/src/main/java/com/stjohns/stormchat/Activities/SearchActivity.java
+++ app/src/main/java/com/stjohns/stormchat/Activities/SearchActivity.java
@@ -2,6 +2,9 @@ package com.stjohns.stormchat.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -26,5 +29,27 @@ public class SearchActivity extends Activity {
                 arraySearchResults);
         lv.setAdapter(adapter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem item = menu.findItem(R.id.menuSearch);
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
