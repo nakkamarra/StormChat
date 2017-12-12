91fc5b68f6eb10869b30ba46bc97ebfae70b5f09
Maurice Billy <maurice.billy13@stjohns.edu>
2017-12-11 22:41:35 -0500

    The chat sort've works

--- app/src/main/java/com/stjohns/stormchat/Activities/ChatActivity.java
+++ app/src/main/java/com/stjohns/stormchat/Activities/ChatActivity.java
@@ -1,114 +1,173 @@
 package com.stjohns.stormchat.Activities;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.stjohns.stormchat.Objects.Chat.Chat;
-import com.stjohns.stormchat.Objects.User.User;
 import com.stjohns.stormchat.R;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class ChatActivity extends Activity
 {
 
     private EditText userMessage;
-    private TextView date, user;
-    private DatabaseReference mDatabase;
-    static ListView listview;
-    static ArrayList<String> list = new ArrayList<>();
-    ArrayAdapter<String> adapter;
-    static final User[] users = {new User()};
+    private DatabaseReference reference;
+    private FirebaseDatabase database;
+
+    Message mess;
+
+    private ArrayList<String> list = new ArrayList<>();
+
+    private RecyclerView recyclerView;
+    private UserAdapter adapter;
+    private List<Message> result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_activity);
 
-        listview = (ListView) findViewById(R.id.listView);
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Messages");
-        adapter = new ArrayAdapter<>(this,android.R.layout.simple_dropdown_item_1line, list);
-        listview.setAdapter(adapter);
-        mDatabase.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                list.add(dataSnapshot.child("Content").getValue().toString());
-                adapter.notifyDataSetChanged();
-            }
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-            }
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-                list.remove(dataSnapshot.getValue(String.class));
-                adapter.notifyDataSetChanged();
-            }
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("Messages");
 
-        DatabaseReference text = FirebaseDatabase.getInstance().getReference().child("Users");
 
-        text.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                users[0] = dataSnapshot.getValue(User.class);
-            }
+        result = new ArrayList<>();
+        recyclerView = (RecyclerView) findViewById(R.id.listView);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager lin = new LinearLayoutManager(this);
+        lin.setStackFromEnd(true);
+        lin.setOrientation(LinearLayoutManager.VERTICAL);
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+        recyclerView.setLayoutManager(lin);
 
-            }
-        });
+
+        adapter = new UserAdapter(result);
+        recyclerView.setAdapter(adapter);
+
+        updateList();
+
+//        mDatabase.addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                list.add(dataSnapshot.child("Content").getValue().toString());
+//                adapter.notifyDataSetChanged();
+//            }
+//            @Override
+//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//            }
+//            @Override
+//            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//                list.remove(dataSnapshot.getValue(String.class));
+//                adapter.notifyDataSetChanged();
+//            }
+//            @Override
+//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//            }
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//            }
+//        });
 
 
         userMessage = (EditText) findViewById(R.id.editMessage);
-        date = (TextView) findViewById(R.id.date);
-        user = (TextView) findViewById(R.id.userName);
+//        date = (TextView) findViewById(R.id.date);
+//        user = (TextView) findViewById(R.id.userName);
     }
-    //
+
+
     public void sendButtonClicked(View view) {
         final String messageValue = userMessage.getText().toString().trim();
-        //final String dateSent = date.getText().toString().trim();
-        //final String userName = user.getText().toString().trim();
+
+        final String date =  DateFormat.getDateTimeInstance().format(new Date());
+        final String userName = "Maurice";
+        mess = new Message(messageValue, date, userName);
         if (!TextUtils.isEmpty(messageValue)) {
-            final DatabaseReference newPost = mDatabase.push();
-            newPost.child("Message").setValue(messageValue);
-            //newPost.child("Date").setValue(dateSent);
-            //newPost.child("Received User").setValue(userName);
+            final DatabaseReference newPost = reference.push();
+//            newPost.child("Content").setValue(messageValue);
+//            newPost.child("Date").setValue(date);
+//            newPost.child("userName").setValue(userName);
+            newPost.setValue(mess);
+
         }
+    }
+
+    private void updateList() {
+        reference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                String content = ""+dataSnapshot.child("Content").getValue().toString();
+//                String date = ""+dataSnapshot.child("Date").getValue();
+//                String userName = ""+dataSnapshot.child("userName").getValue();
+//                mess = new Message(content, date, userName);
+//                result.add(new Message(content, date, userName));
+                result.add(dataSnapshot.getValue(Message.class));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//                Message message = dataSnapshot.getValue(Message.class);
+
+//                int index = getItemIndex(mess);
+//
+//                result.set(index, mess);
+//                adapter.notifyItemChanged(index);
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//                Message message = dataSnapshot.getValue(Message.class);
+//
+//                int index = getItemIndex(message);
+//
+//                result.remove(index);
+//                adapter.notifyItemRemoved(index);
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.i("Error",""+databaseError.getMessage());
+            }
+        });
+    }
+
+
+    private int getItemIndex(Message message) {
+
+        int index =-1;
+
+        for (int i = 0; i<result.size(); i++) {
+            if (result.get(i).getContent().equals(message.getContent()) ) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
     }
 
     @Override
     public void onStart()
     {
         super.onStart();
-        Log.e("User", users[0].toString());
-    }
-
-
-    @Override
-    public void onBackPressed(){
-        Intent whereToGo = new Intent(ChatActivity.this, HomeActivity.class);
-        ChatActivity.this.finish();
-        startActivity(whereToGo);
     }
 }
